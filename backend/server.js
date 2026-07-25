614183253943fb8de2981bb141beffaf9580ac81
AKASH7455 <ubayveer5@gmail.com>
2026-07-25 11:46:33 +0530

    add railway.toml

--- backend/server.js
+++ backend/server.js
@@ -3,7 +3,7 @@ require("dotenv").config();
 const app = require("./app");
 const connectDatabase = require("./config/database");
 
-const PORT = process.env.PORT;
+const PORT = process.env.PORT || 5000;
 
 const startServer = async () => {
   try {
